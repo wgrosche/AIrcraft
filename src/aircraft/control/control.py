b0e632ab56c25223835fe330606c1840b7f08da6
wgrosche <61826753+wgrosche@users.noreply.github.com>
2025-03-19 14:15:27 +0100

    minor shuffle

--- src/aircraft/control/control.py
+++ src/aircraft/control/control.py
@@ -331,7 +331,7 @@ class AircraftControl(ControlProblem):
         """
         super()._setup_objective(nodes)
         
-        final_waypoint = [0, 100, -100]
+        final_waypoint = [0, 20, -190]
 
         # tolerance = 2 * np.linalg.norm(np.array([0,0, -200]) - np.array(final_waypoint)) / (self.num_nodes - 1)# TODO: Change from hardcoding to deriving from the initial position and the goal
         final_waypoint_diff = nodes[-1].state[:3] - final_waypoint
@@ -535,70 +535,13 @@ class QuadrotorControl(ControlProblem):
         self.opti.subject_to(self.state[:3, -1] ==  [100, 100, 100])
         self.opti.minimize(self.loss(time = self.time))
 
-    def solve(self):
-        sol = super().solve()
-        # states = sol.value()['xf']
+    def solve(self, warm_start:Optional[ca.OptiSol] = None):
+        sol = super().solve(warm_start = warm_start)
         fig = plt.figure(figsize=(10, 8))
         ax = fig.add_subplot(111, projection='3d')
         ax.scatter(sol.value(self.state[0, :]), sol.value(self.state[1, :]), sol.value(self.state[2, :]))
         plt.show(block = True)
 
-
-class Quadrotor:
-    def __init__(self):
-        """
-        RPG Time Optimal Quadrotor Simplification for testing
-        """
-        self.I = ca.DM([(1, 0, 0), (0, 1, 0), (0, 0, 1)])    # Inertia
-        self.I_inv = ca.inv(self.I) 
-        self.g = -9.81
-        self.m = 1.0
-
-    @property
-    def dynamics(self):
-        
-        p = ca.MX.sym('p', 3)
-        v = ca.MX.sym('v', 3)
-        q = ca.MX.sym('q', 4)
-        w = ca.MX.sym('w', 3)
-        T = ca.MX.sym('thrust', 4)
-
-        x = ca.vertcat(p, v, q, w)
-        u = ca.vertcat(T)
-
-        g = ca.DM([0, 0, -self.g])
-        q = Quaternion(q)
-        thrust_quat = Quaternion(ca.vertcat(0, 0, (T[0]+T[1]+T[2]+T[3]), 0))
-        rotated_thrust = (q * thrust_quat * q.inverse()).coeffs()[:3]
-        ang_acc_quat = Quaternion(ca.vertcat(w, 0))
-        x_dot = ca.vertcat(
-                        v,
-                        rotated_thrust + g - v,
-                        0.5 * q * ang_acc_quat * q.inverse(),
-                        ca.mtimes(self.I_inv, ca.vertcat(
-                                    (T[0]-T[1]-T[2]+T[3]),
-                                    (-T[0]-T[1]+T[2]+T[3]),
-                                    0.5*(T[0]-T[1]+T[2]-T[3]))
-                        -ca.cross(w,ca.mtimes(self.I,w)))
-        )
-        fx = ca.Function('f',  [x, u], [x_dot], ['x', 'u'], ['x_dot'])
-        return fx
-    
-    
-    def step(self):
-        dt = ca.MX.sym('dt', 1)
-        x = ca.MX.sym('x', self.dynamics.size1_in(0))
-        u = ca.MX.sym('u', self.dynamics.size1_in(1))
-        k1 = self.dynamics(x, u)
-        k2 = self.dynamics(x + dt/2 * k1, u)
-        k3 = self.dynamics(x + dt/2 * k2, u)
-        k4 = self.dynamics(x + dt * k3, u)
-        integrator = ca.Function('integrator',
-            [x, u, dt],
-            [x + dt/6 * (k1 + 2*k2 + 2*k3 + k4)],
-            ['x', 'u', 'dt'], ['xn'])
-        return integrator
-    
 class MHE(ControlProblem):
     def __init__(self, reference_trajectory):
         super().__init__()
@@ -612,7 +555,7 @@ class MHE(ControlProblem):
     def _setup_objective(self, nodes):
         opti = self.opti
 
-        return 
+        return None
     
 import casadi as ca
 import numpy as np
@@ -719,12 +662,12 @@ class MHE:
         return x_traj, u_traj, s_traj
 
 
-def minimal_quad_test():
-    quad = Quadrotor()
-    control_problem = QuadrotorControl(quad.step())
+# def minimal_quad_test():
+#     quad = Quadrotor()
+#     control_problem = QuadrotorControl(quad.step())
 
-    control_problem.setup()
-    control_problem.solve()
+#     control_problem.setup()
+#     control_problem.solve()
 
     
 
--- src/aircraft/dynamics/dynamics.py
+++ src/aircraft/dynamics/dynamics.py
@@ -813,7 +813,8 @@ class AircraftLinear(SixDOF):
         return ca.Function('coefficients', [self.state, self.control], [self._coefficients])
     
 class Quadrotor(SixDOF):
-    super().__init__()
+    def __init__(self):
+        super().__init__()
 
     @property
     def control(self):
@@ -861,7 +862,61 @@ class Quadrotor(SixDOF):
     
 
 
+# class Quadrotor:
+#     def __init__(self):
+#         """
+#         RPG Time Optimal Quadrotor Simplification for testing
+#         """
+#         self.I = ca.DM([(1, 0, 0), (0, 1, 0), (0, 0, 1)])    # Inertia
+#         self.I_inv = ca.inv(self.I) 
+#         self.g = -9.81
+#         self.m = 1.0
 
+#     @property
+#     def dynamics(self):
+        
+#         p = ca.MX.sym('p', 3)
+#         v = ca.MX.sym('v', 3)
+#         q = ca.MX.sym('q', 4)
+#         w = ca.MX.sym('w', 3)
+#         T = ca.MX.sym('thrust', 4)
+
+#         x = ca.vertcat(p, v, q, w)
+#         u = ca.vertcat(T)
+
+#         g = ca.DM([0, 0, -self.g])
+#         q = Quaternion(q)
+#         thrust_quat = Quaternion(ca.vertcat(0, 0, (T[0]+T[1]+T[2]+T[3]), 0))
+#         rotated_thrust = (q * thrust_quat * q.inverse()).coeffs()[:3]
+#         ang_acc_quat = Quaternion(ca.vertcat(w, 0))
+#         x_dot = ca.vertcat(
+#                         v,
+#                         rotated_thrust + g - v,
+#                         0.5 * q * ang_acc_quat * q.inverse(),
+#                         ca.mtimes(self.I_inv, ca.vertcat(
+#                                     (T[0]-T[1]-T[2]+T[3]),
+#                                     (-T[0]-T[1]+T[2]+T[3]),
+#                                     0.5*(T[0]-T[1]+T[2]-T[3]))
+#                         -ca.cross(w,ca.mtimes(self.I,w)))
+#         )
+#         fx = ca.Function('f',  [x, u], [x_dot], ['x', 'u'], ['x_dot'])
+#         return fx
+    
+    
+#     def step(self):
+#         dt = ca.MX.sym('dt', 1)
+#         x = ca.MX.sym('x', self.dynamics.size1_in(0))
+#         u = ca.MX.sym('u', self.dynamics.size1_in(1))
+#         k1 = self.dynamics(x, u)
+#         k2 = self.dynamics(x + dt/2 * k1, u)
+#         k3 = self.dynamics(x + dt/2 * k2, u)
+#         k4 = self.dynamics(x + dt * k3, u)
+#         integrator = ca.Function('integrator',
+#             [x, u, dt],
+#             [x + dt/6 * (k1 + 2*k2 + 2*k3 + k4)],
+#             ['x', 'u', 'dt'], ['xn'])
+#         return integrator
+ 
 
 class AircraftTrim(Aircraft):
     def __init__(
