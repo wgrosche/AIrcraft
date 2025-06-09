f36a186c2de84bbafa2869c0c3e27beb8e498b71
wgrosche <wgrosche@student.ethz.ch>
2025-06-09 10:31:05 +0200

    Update moving_horizon.py

--- src/aircraft/control/moving_horizon.py
+++ src/aircraft/control/moving_horizon.py
@@ -1,140 +1,37 @@
-# import numpy as np
-# from dataclasses import dataclass
-# import casadi as ca
-# from aircraft.control.base import ControlProblem, ControlNode
-# from aircraft.control.aircraft import AircraftControl
-# from dataclasses import dataclass
-# from typing import Optional, List
-# from aircraft.control.initialisation import DubinsInitialiser
-
-# @dataclass
-# class MPCCNode(ControlNode):
-#     progress:Optional[ca.MX] = None
-
-# class MHTT(ControlProblem):
-#     def __init__(self, *, track, track_length, dt, **kwargs):
-#         super().__init__(**kwargs)
-#         self.track = track
-#         self.dt = dt
-#         self.time = dt * self.num_nodes
-#         self.track_length = track_length
-#         self.initial_progress_param = self.opti.parameter()
-    
-#     def loss(self, nodes, time = None):
-#         return self.progress_loss
-
-#     def _setup_time(self):
-#         """
-#         Override as we don't need variable timestepping here. 
-#         """
-#         pass
-
-#     def _setup_initial_node(self, guess):
-#         """
-#         set up the progress variable.
-#         """
-#         current_node = super()._setup_initial_node(guess)
-#         progress = self.opti.variable()
-#         self.progress = [progress]
-#         tracking_error = ca.sumsqr(self.track(progress) - current_node.state[:3])
-#         self.constraint(progress == self.initial_progress, description="Initial Progress Lock")
-
-#         self.progress_loss = 1000 * tracking_error - 100 * progress
-
-#         return current_node
-    
-#     def _setup_step(self, index, current_node, guess):
-#         print(current_node)
-#         current_node = super()._setup_step(index = index, current_node = current_node, guess = guess)
-#         progress = self.opti.variable()
-#         self.progress.append(progress)
-#         tracking_error = ca.sumsqr(self.track(progress) - current_node.state[:3])
-#         self.progress_loss += 100 * tracking_error - 10 * progress
-#         self.constraint(self.progress[-1] >= self.progress[-2], description="Progress Constraint")
-#         self.constraint(self.opti.bounded(0, self.progress[-1], 1), description="Progress Constraint")
-#         return current_node
-
-#     def _setup_variables(self, nodes:List[ControlNode]):
-#         super()._setup_variables(nodes)
-#         self.progress = ca.vcat(self.progress)
-
-#         if self.verbose:
-#             print("Progress Shape: ", self.progress.shape)
-
-#     def setup(self, guess, progress):
-#         super().setup(guess)
-#         self.opti.set_initial(self.progress, progress)
-
-#     # def initialise(self, initial_state):
-#     #     guess = np.zeros((self.state_dim + self.control_dim, self.num_nodes + 1))
-#     #     guess[:self.state_dim, 0] = initial_state.toarray().flatten()
-
-#     #     for i in range(self.num_nodes):
-#     #         guess[:self.state_dim, i + 1] = self.dynamics(guess[:self.state_dim, i], guess[self.state_dim:, i], self.dt).toarray().flatten()
-
-#     #     progress = [0.0]
-#     #     for i in range(1, self.num_nodes + 1):
-#     #         dx = guess[:3, i] - guess[:3, i - 1]
-#     #         progress.append(progress[-1] + np.linalg.norm(dx))
-#     #     progress = np.array(progress)
-#     #     progress /= progress[-1]  # Normalize to [0, 1]
-
-
-#     #     return guess, progress
-
-#     def initialise(self, initial_state, current_progress):
-#         guess = np.zeros((self.state_dim + self.control_dim, self.num_nodes + 1))
-#         guess[:self.state_dim, 0] = initial_state.toarray().flatten()
-        
-#         # Propagate forward using nominal dynamics
-#         for i in range(self.num_nodes):
-#             guess[:self.state_dim, i + 1] = self.dynamics(
-#                 guess[:self.state_dim, i],
-#                 guess[self.state_dim:, i],
-#                 self.dt
-#             ).toarray().flatten()
-
-#         # Estimate progress over the horizon, starting from current_progress
-#         progress = [current_progress]
-#         for i in range(1, self.num_nodes + 1):
-#             dx = guess[:3, i] - guess[:3, i - 1]
-#             delta = np.linalg.norm(dx) / self.track_length  # normalize relative to full path
-#             progress.append(progress[-1] + delta)
-#         self.initial_progress = current_progress
-
-#         return guess, np.array(progress)
-
-    
-#     def callback(self, iteration:int):
-#         """
-#         To be implemented
-#         """
-#         pass
-
 import numpy as np
 from dataclasses import dataclass
 import casadi as ca
 from aircraft.control.base import ControlProblem, ControlNode
-# from aircraft.control.aircraft import AircraftControl
 from dataclasses import dataclass
 from typing import Optional, List
 from aircraft.control.initialisation import DubinsInitialiser
 from aircraft.plotting.plotting import TrajectoryData
 import matplotlib.pyplot as plt
 
+
 @dataclass
-class MPCCNode(ControlNode):
-    progress: Optional[ca.MX] = None
+class ProgressNode(ControlNode):
+    track_progress: Optional[ca.MX] = None
+
+    @classmethod
+    def from_control_node(cls, control_node: ControlNode, track_progress: Optional[ca.MX] = None):
+        return cls(
+            index=control_node.index,
+            state=control_node.state,
+            control=control_node.control,
+            progress=control_node.progress,
+            track_progress=track_progress
+        )
 
 class MHTT(ControlProblem):
     def __init__(self, *, track, track_length, dt, **kwargs):
+        assert kwargs.get('opts').get('time', 'fixed') == 'fixed', 'can only run mhtt with fixed time'
         super().__init__(**kwargs)
         self.track = track
         self.dt = dt
         self.time = dt * self.num_nodes
         self.track_length = track_length
         
-        # Create parameters for changing values between iterations
         self.initial_progress_param = self.opti.parameter()
         self.initial_state_param = self.opti.parameter(self.state_dim)
 
@@ -145,54 +42,47 @@ class MHTT(ControlProblem):
     def loss(self, nodes, time=None):
         return self.progress_loss
 
-    def _setup_time(self):
-        """
-        Override as we don't need variable timestepping here. 
-        """
-        pass
-
     def _setup_initial_node(self, guess):
         """
         Set up the progress variable and use parameter for initial state.
         """
-        current_node = super()._setup_initial_node(guess)
+        current_node = self._make_node(index=0, guess=guess, enforce_state_constraint=False)
+        # current_node = super()._setup_initial_node(guess)
         
         # Constrain initial state to parameter value
         self.constraint(current_node.state == self.initial_state_param, description="Initial State Lock")
-        
-        # Use parameter for progress
-        progress = self.opti.variable()
-        self.progress = [progress]
-        tracking_error = ca.sumsqr(self.track(progress) - current_node.state[:3])
-        self.constraint(progress == self.initial_progress_param, description="Initial Progress Lock")
 
-        self.progress_loss = 10 * tracking_error - 1* progress
+        self.constraint(current_node.track_progress == self.initial_progress_param, description="Initial Progress Lock")
 
         return current_node
     
-    def _setup_step(self, index, current_node, guess):
-        current_node = super()._setup_step(index=index, current_node=current_node, guess=guess)
-        progress = self.opti.variable()
-        self.progress.append(progress)
-        tracking_error = ca.sumsqr(self.track(progress) - current_node.state[:3])
-        self.progress_loss += 100 * tracking_error - 10 * progress
-        self.constraint(self.progress[-1] >= self.progress[-2], description="Progress Constraint")
-        self.constraint(self.opti.bounded(0, self.progress[-1], 1), description="Progress Constraint")
+    def _make_node(self, index: int, guess: np.ndarray, enforce_state_constraint: bool = False) -> ProgressNode:
+        control_node = super()._make_node(index, guess, enforce_state_constraint)
+
+        progress_node = ProgressNode.from_control_node(control_node, track_progress=self.opti.variable())
+        return progress_node
+    
+    def _setup_step(self, index, current_node:ProgressNode, guess):
+        next_node:ProgressNode = super()._setup_step(index=index, current_node=current_node, guess=guess)
+
+        self.constraint(next_node.track_progress >= current_node.track_progress, description="Progress Constraint")
+
+        self.constraint(self.opti.bounded(0, next_node.track_progress, 1), description="Progress Constraint")
         return current_node
 
-    def _setup_variables(self, nodes: List[ControlNode]):
+    def _setup_variables(self, nodes: List[ProgressNode]):
         super()._setup_variables(nodes)
-        self.progress = ca.vcat(self.progress)
+        self.track_progress = ca.vcat(*[node.track_progress for node in nodes])
 
         if self.verbose:
-            print("Progress Shape: ", self.progress.shape)
+            print("Progress Shape: ", self.track_progress.shape)
 
     def setup(self, guess, progress, initial_state=None):
         """
         Set up the optimization problem with initial guesses and parameter values.
         """
         super().setup(guess)
-        self.opti.set_initial(self.progress, progress)
+        self.opti.set_initial(self.track_progress, progress)
         
         # Set parameter values if provided
         if initial_state is not None:
@@ -215,17 +105,17 @@ class MHTT(ControlProblem):
             ).toarray().flatten()
 
         # Estimate progress over the horizon, starting from current_progress
-        progress = [current_progress]
-        for i in range(1, self.num_nodes + 1):
-            dx = guess[:3, i] - guess[:3, i - 1]
-            delta = np.linalg.norm(dx) / self.track_length  # normalize relative to full path
-            progress.append(progress[-1] + delta)
+        # progress = [current_progress]
+        # for i in range(1, self.num_nodes + 1):
+        #     dx = guess[:3, i] - guess[:3, i - 1]
+        #     delta = np.linalg.norm(dx) / self.track_length  # normalize relative to full path
+        #     progress.append(progress[-1] + delta)
             
         # Set parameter values
         self.opti.set_value(self.initial_state_param, initial_state)
         self.opti.set_value(self.initial_progress_param, current_progress)
 
-        return guess, np.array(progress)
+        return guess#, np.array(progress)
     
     def update_parameters(self, initial_state, current_progress):
         """
@@ -255,6 +145,7 @@ class MHTT(ControlProblem):
     #         plt.draw()
     #         self.plotter.figure.canvas.start_event_loop(0.0002)
     def callback(self, iteration: int):
+        pass
         # Plot full trajectory every N iterations
         if self.plotter and iteration % 10 == 5:
             current_state = self.opti.debug.value(self.state)[:, 1:]
